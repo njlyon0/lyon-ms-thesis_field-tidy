1acc411779b72e459c6efbab17dd0a33510229e4
njlyon0 <njlyon@alumni.iastate.edu>
2024-10-30 15:23:13 -0400

    feat (bee proj): script skeleton for wrangling bee data

---
+++ bee-project_tidy-bees.R
@@ -0,0 +1,33 @@
+##  ------------------------------------------------------------  ##
+                  # Bee Project - Bee Tidy 2017-2018
+##  ------------------------------------------------------------  ##
+# Written by Nick J Lyon
+
+# Script purpose:
+## Wrangle 2017 and 2018 data on native bees in southern Iowa/northern Missouri
+
+##  ------------------------------------------  ##      
+# Housekeeping ----
+##  ------------------------------------------  ##      
+
+# Set required libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, vegan)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+##  ------------------------------------------  ##      
+# 2017 Standardization ----
+##  ------------------------------------------  ##      
+
+
+##  ------------------------------------------  ##      
+# 2018 Standardization ----
+##  ------------------------------------------  ##      
+
+
+
+
+
+# End ----
