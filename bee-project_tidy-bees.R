693b3a2a7cc97e421ccd64fd9b381c6ef1b1fdb6
njlyon0 <njlyon@alumni.iastate.edu>
2024-10-30 16:14:53 -0400

    feat (bee proj): 2018 bee data wrangling code

--- bee-project_tidy-bees.R
+++ bee-project_tidy-bees.R
@@ -18,7 +18,7 @@ librarian::shelf(tidyverse, vegan)
 rm(list = ls()); gc()
 
 ##  ------------------------------------------  ##      
-# 2017 Standardization ----
+          # 2017 Standardization ----
 ##  ------------------------------------------  ##      
 
 # Read in data
@@ -81,16 +81,109 @@ bz.17_v3 <- bz.17_v2 %>%
   # Then drop that column
   dplyr::select(-bowls.recovered_percent)
 
+# Final structure check
+dplyr::glimpse(bz.17_v3)
+
 # Export this tidied data!
 write.csv(x = bz.17_v3, row.names = F, na = '',
           file = file.path("data", "tidy-data", "bee-project_tidy-bees_2017.csv"))
 
 ##  ------------------------------------------  ##      
-# 2018 Standardization ----
+            # 2018 Standardization ----
 ##  ------------------------------------------  ##      
 
+# Read in data
+bz.18_v0 <- read.csv(file = file.path("data", "raw-data", "bee-project_raw-bees_2018.csv"))
 
+# Check structure
+dplyr::glimpse(bz.18_v0)
 
+# Simplify/standardize column names
+bz.18_v1 <- bz.18_v0 %>% 
+  # Drop unwanted rows
+  dplyr::filter(is.na(Capture.Year) != T) %>% 
+  # Generate new needed columns
+  ## Quantitative 'height'
+  dplyr::mutate(height_cm = ifelse(test = (Height == "Low"),
+                                   yes = 2.5, no = 100),
+                .after = Height) %>% 
+  ## Quantitative 'bowl size'
+  dplyr::mutate(bowl.size_oz = as.numeric(gsub(pattern = "oz", replacement = "",
+                                               x = Bowl.Size)),
+                .after = Bowl.Size) %>% 
+  ## Better 'species' column
+  dplyr::mutate(species = paste0(Genus, ".", Species),
+                .after = Species) %>% 
+  # Drop (some) old columns
+  dplyr::select(-Height, -Species, -Bowl.Size, -ID.Check, -Pinned) %>% 
+  # Rename columns
+  dplyr::rename(sampling.event.id = Sampling.Event.ID,
+                sampling.round = Round,
+                capture.year = Capture.Year,
+                capture.date = Capture.Date,
+                pasture = Site,
+                patch = Patch,
+                bowl.position = Bowl.Position,
+                bowl.color = Bowl.Color,
+                bowl.status = Bowl.Status,
+                specimen.id = Specimen.ID,
+                family = Family,
+                genus = Genus,
+                sex = Sex,
+                number = Number)
+
+# Check structure
+dplyr::glimpse(bz.18_v1)
+
+# QC column contents
+bz.18_v2 <- bz.18_v1 %>% 
+  # Remove casing from some columns
+  dplyr::mutate(dplyr::across(.cols = dplyr::all_of(c("bowl.color", "bowl.status")),
+                              .fns = tolower)) %>% 
+  # Fix color typos
+  dplyr::mutate(bowl.color = dplyr::case_when(
+    bowl.color %in% c("bue") ~ "blue",
+    bowl.color %in% c(" yellow", "yellow ", "yelow") ~ "yellow",
+    bowl.color %in% c("whie") ~ "white",
+    T ~ bowl.color)) %>% 
+  # Simplify some bee IDs
+  dplyr::mutate(species = dplyr::case_when(
+    species %in% c("Andrena.cressonii cressonii") ~ "Andrena.cressonii",
+    genus == "Lasioglossum" ~ "Lasioglossum.sp",
+    T ~ species)) %>% 
+  # Remove bowls that weren't simply "retrieved"
+  dplyr::filter(!bowl.status %in% c("off clip", "destroyed")) %>% 
+  dplyr::select(-bowl.status) %>% 
+  # Calculate recovery percentage per height & transect
+  dplyr::group_by(sampling.event.id, height_cm) %>% 
+  dplyr::mutate(bowls.recovered_ct = length(unique(bowl.position)),
+                bowls.recovered_percent = (bowls.recovered_ct / 6) * 100) %>% 
+  dplyr::ungroup()
+
+# Check structure
+dplyr::glimpse(bz.18_v2)
+
+# Do some QC on the remaining rows
+bz.18_v3 <- bz.18_v2 %>%
+  # Remove 'bad' species
+  dplyr::filter(!species %in% c("X.x", ".", "Andrena.sp", "Hylaeus.sp")) %>% 
+  # Remove all bowls if less than 80% of the bowls were recovered
+  dplyr::filter(bowls.recovered_percent >= 80) %>% 
+  # Then drop that column
+  dplyr::select(-dplyr::starts_with("bowls.recovered_")) %>% 
+  # Summarize bees within critical columns
+  dplyr::group_by(capture.year, capture.date, pasture, patch, height_cm,
+                  bowl.color, bowl.size_oz, family, genus, species) %>% 
+  dplyr::summarize(bee.total = sum(number, na.rm = T),
+                   .groups = "keep") %>% 
+  dplyr::ungroup()
+
+# Final structure check
+dplyr::glimpse(bz.18_v3)
+
+# Export this tidied data!
+write.csv(x = bz.18_v3, row.names = F, na = '',
+          file = file.path("data", "tidy-data", "bee-project_tidy-bees_2018.csv"))
 
 
 # End ----
