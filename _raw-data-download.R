98196cbab6dfefdbab05d1cfeebfbb26e59833a7
njlyon0 <njlyon@alumni.iastate.edu>
2024-10-31 15:34:26 -0400

    feat: data download script (from Google Drive)

---
+++ _raw-data-download.R
@@ -0,0 +1,49 @@
+##  ------------------------------------------------------------  ##
+                  # All Projects - Download Data
+##  ------------------------------------------------------------  ##
+# Written by Nick J Lyon
+
+# Script purpose:
+## Download raw data from Google Drive folder
+
+##  ------------------------------------------  ##      
+                # Housekeeping ----
+##  ------------------------------------------  ##      
+
+# Set required libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, googledrive)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+# Create needed folder(s)
+dir.create(path = file.path("data"), showWarnings = F)
+dir.create(path = file.path("data", "raw-data"), showWarnings = F)
+
+# Identify relevant Drive folder
+drive_url <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1PBA3pYOvpK1CSWS0FUxzBVooU3i1ZeJS")
+
+##  ------------------------------------------  ##      
+                # Download Data ----
+##  ------------------------------------------  ##      
+
+# Identify files that are already downloaded
+already_done <- dir(file.path("data", "raw-data"))
+
+# List files in Drive
+all_data <- googledrive::drive_ls(path = drive_url)
+
+# Identify only the needed files
+needed_data <- all_data  %>% 
+  ## Remove unwanted files/folders
+  dplyr::filter(!name %in% c("_data-dictionaries")) %>% 
+  ## And remove already downloaded files
+  dplyr::filter(!name %in% already_done)
+
+# Download the needed files
+purrr::walk2(.x = needed_data$id, .y = needed_data$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                                path = file.path("data", "raw-data", .y)))
+
+# End ----
