4de5abb435ace075cd2059be45e1159d2a8f916b
njlyon0 <njlyon@alumni.iastate.edu>
2024-10-31 10:29:37 -0400

    feat (daub proj): daubenmire wrangling script skeleton

---
+++ daubenmire-project_tidy-plants.R
@@ -0,0 +1,24 @@
+##  ------------------------------------------------------------  ##
+          # Daubenmire Project - Plant Quadrat Tidying
+##  ------------------------------------------------------------  ##
+# Written by Nick J Lyon
+
+# Script purpose:
+## Wrangle data on plant functional groups in southern Iowa/northern Missouri
+
+##  ------------------------------------------  ##      
+# Housekeeping ----
+##  ------------------------------------------  ##      
+
+# Set required libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+
+
+
+
+# End ----
